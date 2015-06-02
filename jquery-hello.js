6c41fae456bc209c8f98f30bedc3028ee380f5ac
Valentín <htmgarcia@gmail.com>
2015-06-02 17:02:14 -0500

    Starter commit

---
+++ jquery-hello.js
@@ -0,0 +1,16 @@
+(function ( $ ) {
+ 
+    $.fn.hello = function( options ) {
+ 
+        // Default options
+        var settings = $.extend({
+            name: 'John Doe',
+            color: 'orange'
+        }, options );
+ 
+        // Apply options
+        return this.append('Hello ' + settings.name + '!').css({ color: settings.color });
+ 
+    };
+ 
+}( jQuery ));
